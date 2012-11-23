a9ff116b30ebadbd70e110aca53af26a32ba88e8
mockturtl <mockturtl@gmail.com>
2012-11-22 23:48:09 -0500

    add error handling

--- newton.go
+++ newton.go
@@ -1,27 +1,45 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
 
-func Sqrt(x int, z float64) (float64, float64) {
+const precision = 20
+
+func Sqrt(x int, z float64) (out float64, delta float64, err error) {
+	if x < 0 {
+		out, delta = math.NaN(), math.NaN()
+		err = errors.New(fmt.Sprint("cannot Sqrt negative number: ", x))
+		return
+	}
+
 	i := 0
-	delta := math.Abs(math.Sqrt(float64(x)) - z)
-	epsilon := 1e-13
+	const epsilon = 1e-13
+	delta = math.Abs(math.Sqrt(float64(x)) - z)
 	for delta > epsilon {
-		fmt.Printf("guess %d: %.20f\n", i, z)
+		fmt.Printf("guess %d: %.*f\n", i, precision, z)
 		i++
 		z = (z*z + float64(x)) / (2 * z)
 		delta = math.Abs(math.Sqrt2 - z)
 	}
-	return z, delta
+	out = z
+	return
 }
 
 func main() {
-	answer, delta := Sqrt(2, 1.5)
-	fmt.Printf("computed: %.20f, actual: %.20f, delta: %.20f\n",
+	answer, delta, err := Sqrt(2, 1.5)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("computed: %.*f, actual: %.*f, delta: %.*f\n",
+		precision,
 		answer,
+		precision,
 		math.Sqrt2,
+		precision,
 		delta)
 }
