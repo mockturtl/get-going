2c9ae3201c1979a7c2270600407eb3c35f99edaf
mockturtl <mockturtl@gmail.com>
2012-11-25 14:24:38 -0500

    explicit race output

--- wordcount.go
+++ wordcount.go
@@ -2,20 +2,30 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"strings"
+	"time"
 )
 
-func WordCount(s string, c chan map[string]int) {
+func SleepRand(index int) {
+	r := rand.Intn(1000)
+	fmt.Printf("#%d Sleeping for %d ms...\n", index, r)
+	time.Sleep(time.Duration(r) * time.Millisecond)
+}
+
+func WordCount(s string, c chan map[string]int, index int) {
 	arr := strings.Fields(s)
 	out := make(map[string]int)
 	for _, v := range arr {
 		out[v]++
 	}
-	fmt.Println(arr)
+	fmt.Printf("#%d %q\n", index, arr)
+	SleepRand(index) // sleepy routines finish last!
 	c <- out
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	tests := []string{
 		"I am learning Go!",
 		"The quick brown fox jumped over the lazy dog.",
@@ -25,8 +35,8 @@ func main() {
 
 	c := make(chan map[string]int, len(tests))
 
-	for _, v := range tests {
-		go WordCount(v, c) // races!
+	for k, v := range tests {
+		go WordCount(v, c, k) // race!
 	}
 
 	for i := 0; i < len(tests); i++ {
